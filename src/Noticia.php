df721bb06df0f698f479f54917c4c8a991a1e917
victor bueno <victorhugomotabueno@gmail.com>
2023-10-26 08:41:24 -0300

    Método atualizar e o algoritmo para atualizar foto

--- src/Noticia.php
+++ src/Noticia.php
@@ -123,6 +123,42 @@ class Noticia
         return $resultado;
     }
 
+
+    public function atualizar():void {
+        if ( $this->usuario->getTipo() === "admin" ) {
+            // Carrega dados de qualquer noticia de qualquer pessoa
+            $sql = "UPDATE noticias SET titulo = :titulo, texto = :texto, resumo = :resumo,
+                    imagem = :imagem, categoria_id = :categoria_id,
+                    destaque = :destaque WHERE id = :id";
+        } else {
+            // Carrega dados de qualquer noticia DELE/DELA
+            $sql = "UPDATE noticias SET titulo = :titulo, texto = :texto, resumo = :resumo,
+                    imagem = :imagem, categoria_id = :categoria_id,
+                    destaque = :destaque WHERE id = :id
+                    AND usuario_id = :usuario_id";
+        }
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":titulo", $this->titulo, PDO::PARAM_STR);
+            $consulta->bindValue(":texto", $this->texto, PDO::PARAM_STR);
+            $consulta->bindValue(":resumo", $this->resumo, PDO::PARAM_STR);
+            $consulta->bindValue(":imagem", $this->imagem, PDO::PARAM_STR);
+            $consulta->bindValue(":destaque", $this->destaque, PDO::PARAM_STR);
+            
+            /* Aqui, primeiro chamamos os getters de ID o Usuario e de Categoria,
+            para só depois associar os valores aos parâmetros da consulta SQL.
+            Isso é possível devido à associação entre as Classes. */
+            $consulta->bindValue(":usuario_id", $this->usuario->getId(), PDO::PARAM_INT);
+            $consulta->bindValue(":categoria_id", $this->categoria->getId(), PDO::PARAM_INT);
+
+            $consulta->execute();
+        } catch (Exception $e) {
+            die("Erro ao inserir notícia: ".$e->getMessage());
+        }
+    }
+
+
     public function upload(array $arquivo):void {
         // Definindo os tipos válidos 
         $tiposValidos = [
